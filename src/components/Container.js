e5ebdf8765fbe4e9570d3f2e6992f644b5d57a37
Matheus Almeida <mat.almeida@live.com>
2019-01-16 20:30:54 -0300

    custom font

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,36 +1,58 @@
 import React from "react";
 import { View } from "react-native";
+import { Font } from "expo";
 import RoundedBorder from "./objects/RoundedBorder";
 import SquaredBorder from "./objects/SquaredBorder";
 import Layout from "../constants/Layout";
 
-function Container({ width = 1, children, center, dark, round, title }) {
-  const componentWidth = Layout.width * width;
-  const strokeColor = dark ? "white" : "black";
-  const backgroundColor = dark ? "black" : "none";
+class Container extends React.PureComponent {
+  state = { fontLoaded: false };
 
-  return (
-    <View style={{ backgroundColor, padding: 2 }}>
-      {!!round && <RoundedBorder width={width} dark={dark} />}
-      {!round && <SquaredBorder width={width} dark={dark} />}
-      <View
-        style={{
-          margin: 0,
-          padding: 0,
-          width: componentWidth,
-          borderLeftWidth: 4,
-          borderRightWidth: 4,
-          borderColor: strokeColor,
-          paddingHorizontal: 14,
-          alignItems: center && "center"
-        }}
-      >
-        {children}
+  componentDidMount = async () => {
+    await Font.loadAsync({
+      "press-start-2p": require("../../assets/fonts/PressStart2P-Regular.ttf")
+    });
+
+    this.setState({ fontLoaded: true });
+  };
+
+  render() {
+    const { width = 1, children, center, dark, round, title } = this.props;
+    const { fontLoaded } = this.state;
+
+    const componentWidth = Layout.width * width;
+    const strokeColor = dark ? "white" : "black";
+    const backgroundColor = dark ? "black" : "none";
+
+    const newChildren = React.Children.map(children, child => {
+      return React.cloneElement(child, {
+        style: { color: strokeColor, fontFamily: "press-start-2p" }
+      });
+    });
+
+    return (
+      <View style={{ backgroundColor, padding: 2 }}>
+        {!!round && <RoundedBorder width={width} dark={dark} />}
+        {!round && <SquaredBorder width={width} dark={dark} />}
+        <View
+          style={{
+            margin: 0,
+            padding: 0,
+            width: componentWidth,
+            borderLeftWidth: 4,
+            borderRightWidth: 4,
+            borderColor: strokeColor,
+            paddingHorizontal: 14,
+            alignItems: center && "center"
+          }}
+        >
+          {fontLoaded ? newChildren : children}
+        </View>
+        {!!round && <RoundedBorder width={width} dark={dark} flip />}
+        {!round && <SquaredBorder width={width} dark={dark} flip />}
       </View>
-      {!!round && <RoundedBorder width={width} dark={dark} flip />}
-      {!round && <SquaredBorder width={width} dark={dark} flip />}
-    </View>
-  );
+    );
+  }
 }
 
 export default Container;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,29 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Font } from "expo";
 
 import Container from "./components/Container";
 
 function App() {
   return (
     <View
-      style={{ paddingTop: 25, alignItems: "center", justifyContent: "center" }}
+      style={{
+        paddingTop: 25,
+        alignItems: "center",
+        justifyContent: "center"
+      }}
     >
       <Container width={0.9} center>
-        <Text style={{ color: "black" }}>
-          Good morning. Thou hast had a good night's sleep, I hope
-        </Text>
+        <Text>Good morning. Thou hast had a good night's sleep, I hope</Text>
       </Container>
       <Container width={0.9} round>
-        <Text style={{ color: "black" }}>
-          Good morning. Thou hast had a good night's sleep, I hope
-        </Text>
+        <Text>Good morning. Thou hast had a good night's sleep, I hope</Text>
       </Container>
       <Container width={0.9} dark>
-        <Text style={{ color: "white" }}>
-          Good morning. Thou hast had a good night's sleep, I hope
-        </Text>
+        <Text>Good morning. Thou hast had a good night's sleep, I hope</Text>
       </Container>
       <Container width={0.9} dark round>
-        <Text style={{ color: "white" }}>
-          Good morning. Thou hast had a good night's sleep, I hope
-        </Text>
+        <Text>Good morning. Thou hast had a good night's sleep, I hope</Text>
       </Container>
     </View>
   );
